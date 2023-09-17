fc067026200d3514af341c6c9116398d26289e42
mista-a <dvaanddva@gmail.com>
2023-09-17 09:18:16 +0300

    feat: create modal window

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface ModalWindowProps {
+  active: boolean
+  position?: 'top' | 'right' | 'bottom' | 'left'
+  animationDuration?: number
+}
+
+interface ModalWindowEmit {
+  (e: 'close'): void
+}
+
+defineProps<ModalWindowProps>()
+defineEmits<ModalWindowEmit>()
+</script>
+
+<template>
+  <div v-if="active" class="modal" :style="{ animationDuration: `${animationDuration}s` }">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+@keyframes slidein {
+  from {
+    max-width: 0;
+  }
+  to {
+    max-width: 100%;
+  }
+}
+.modal {
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 4;
+  height: 100%;
+  animation-name: slidein;
+}
+</style>
