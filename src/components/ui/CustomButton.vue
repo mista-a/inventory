1c5a0bb786f90e6817d70947386b83339a1e5242
mista-a <dvaanddva@gmail.com>
2023-09-17 09:20:44 +0300

    feat: create custom button

---
+++ src/components/ui/CustomButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border: 0;
+  padding: 0;
+  cursor: pointer;
+  background: inherit;
+}
+.button:active {
+  transform: scale(0.97);
+}
+</style>
