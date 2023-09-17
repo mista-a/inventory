6dc48e6e160493cb9da5d469c4b43b67b06b66a5
mista-a <dvaanddva@gmail.com>
2023-09-17 08:08:27 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="wrapper"></div>
+  </main>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  gap: 24px;
+}
+</style>
